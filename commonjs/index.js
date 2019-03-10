3bcdcb4fb307dd27a03ff9422c6da10913d7a98f
phonemyatkhine <37993997+phonemyatkhine@users.noreply.github.com>
2019-03-10 14:58:04 +0630

    commonjs 1

---
+++ commonjs/index.js
@@ -0,0 +1,3 @@
+var math = require('./math.js');
+
+console.log(math.area(8));
---
+++ commonjs/math.js
@@ -0,0 +1,14 @@
+var PI = 3.14;
+
+function add(a,b) {
+	return a+b;
+}
+
+function area(r) {
+	return PI*r*r;
+}
+
+module.exports = {
+	'PI' : PI,
+	'area' : area
+}
