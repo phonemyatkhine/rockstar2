e4d65bfbf26301a667b87aa461e95f815c166969
phonemyatkhine <37993997+phonemyatkhine@users.noreply.github.com>
2019-03-10 15:53:46 +0630

    array spread

--- es6-features/app.js
+++ es6-features/app.js
@@ -1,4 +1,13 @@
-var PI = 3.14;
+var PI = 3.14; //function
+/*let PI = 3.14; //block scope
+const PI =3.14; //constant 
+*/
+var arr1 = ['a','b','c'];
+var arr2 = ['1','2','3'];
+
+var arr3 = [...arr1,...arr2];
+var arr4 = [...arr3,'d']; //array spread = ...
+console.log(arr4);
 
 function area(r) {
 
